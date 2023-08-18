ed4343f04846227117c21f5a3f6ed8fe392b96a1
akramcheraifia <hebdu636dheb@gmail.com>
2023-08-18 11:37:12 +0100

    added hive db

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/views/notes_view.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  await Hive.openBox('notes');
   runApp(const NoteApp());
 }
 
--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -1,7 +1,15 @@
-class NotesModel {
+import 'package:hive/hive.dart';
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class NotesModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NotesModel(
