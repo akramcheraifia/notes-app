6670a52cacae34858b0ec142055ab650da109657
akramcheraifia <hebdu636dheb@gmail.com>
2023-08-19 11:33:13 +0100

    bloc ovserver added

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/notes_model.dart';
+import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.openBox('notes');
   Hive.registerAdapter(NotesModelAdapter());
   runApp(const NoteApp());
