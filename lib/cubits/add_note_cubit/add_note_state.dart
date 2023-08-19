cf00ccf1fc805c00bdfd156a43d799f0318652bc
akramcheraifia <hebdu636dheb@gmail.com>
2023-08-19 10:16:43 +0100

    add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -0,0 +1,12 @@
+part of 'add_note_cubit.dart';
+
+@immutable
+sealed class AddNoteState {}
+
+final class AddNoteInitial extends AddNoteState {}
+
+final class AddNoteLoading extends AddNoteState {}
+
+final class AddNoteSuccess extends AddNoteState {}
+
+final class AddNoteFailure extends AddNoteState {}
